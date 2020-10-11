e4a8e35e51188ca1a6818919ef29ba5175be1afb
Farshad Mofidi <farshadmofidi970@gmail.com>
2020-10-11 00:03:46 -0700

    strapi

---
+++ BlogServer/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e7aeac7484cf2988627856e458d16f0e'),
+    },
+  },
+});
---
+++ BlogServer/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'daa73480-4794-421a-a261-5aff866239d4'
+};
